0ff06a7874c46d6c095a31fb4b77197d32e27878
ygh547 <ygh547@naver.com>
2022-05-23 14:09:26 +0900

    add

--- app_ml.py
+++ app_ml.py
@@ -1,16 +1,22 @@
 from codecs import getencoder
+from pytest import importorskip
 import streamlit as st
+import numpy as np
+
+import sklearn
 
 import joblib
 
 def run_ml() :
     st.subheader('자동차 구매 가능 금액 예측')
 
+    print(sklearn.__version__)
+
     # 예측하기 위해서 필요한 파일들을 불러와야 된다.
     # 이 예에서는, 인공지능파일, X 스케일러 파일, y 스케일러파일
     # 3개를 불러와야 한다.
 
-    refressor = joblib.load('data/regressor.pkl')
+    regressor = joblib.load('data/regressor.pkl')
     scaler_X = joblib.load('data/scaler_X.pkl')
     scaler_y =joblib.load('data/scaler_y.pkl')
 
@@ -25,6 +31,23 @@ def run_ml() :
     debt = st.number_input('카드빛 입력',0)
     worth = st.number_input('자산 입력',0)
 
-    
+    if st.button('자동차 구매 금액 예측') : 
+
+        # 1. 신규 고객의 정보를 넘파이 어레이로 만들어준다.
+        new_data = np.array([gender,age,Salary,debt,worth])
+
+        # 2. 학습할때 사용한 X 의 피처 스케일러를 이용해서, 피처스케일링하기
+        # 먼저, 데이터를 2차원으로 만들어준다.
+        new_data = new_data.reshape(1, 5)
+        new_data = scaler_X.transform(new_data)
+
+        # 3. 인공지능에게 예측해달라고 한다.
+        y_pred = regressor.predict(new_data)
+        
+        # 4. 예측한 값을, 원상복구 시킨다.
+        y_pred = scaler_y.inverse_transform(y_pred)
+
+        y_pred = round(y_pred[0,0])
+        st.write('이 사람의 구매 가능 금액은'  + str(y_pred) + '달러입니다.')
 
 
